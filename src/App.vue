935f8f3dbfe9cc90df8d1b432a1a8e67ad2b3f8a
omar ibrahim <omar.ibrahim@dynamiktechnologies.com.bn>
2021-03-10 16:29:10 +0800

    feat: create text generator functionality

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Text Generator</h1>
+  <section id="sentence">
+    <label for="sentenceInput">sentence</label><br />
+    <input
+      id="sentenceInput"
+      v-model="sentence"
+      @input="
+        getPlaceholders();
+        replaceSentence();
+      "
+      type="text"
+    />
+  </section>
+  <section id="sentence-preview">
+    <p v-for="(replacedSentence, index) in replacedSentences" :key="index">
+      {{ replacedSentence }}
+    </p>
+  </section>
+  <section id="data">
+    <button @click="replacingWords.push({})">Add</button>
+    <div v-for="(replacingWord, index) in replacingWords" :key="index">
+      <template v-for="(placeholder, index) in placeholders" :key="index">
+        <input
+          v-model="replacingWord[placeholder]"
+          @input="replaceSentence()"
+          type="text"
+        />
+      </template>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from "vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  setup() {
+    const sentence = ref("");
+    const replacedSentences = ref([]);
+    const replacingWords = ref([{}]);
+    const placeholders = ref([]);
+
+    const getPlaceholders = () => {
+      placeholders.value = sentence.value.match(/{([a-zA-Z0-9]*)}/gm);
+    };
+
+    const replaceSentence = () => {
+      replacedSentences.value = [];
+      replacingWords.value.forEach((replacingWord, index) => {
+        replacedSentences.value.push(sentence.value);
+      });
+      placeholders.value.forEach((placeholder) => {
+        replacedSentences.value.forEach((replacedSentence, index) => {
+          replacedSentence = replacedSentence.replace(
+            placeholder,
+            replacingWords.value[index][placeholder]
+          );
+          replacedSentences.value[index] = replacedSentence;
+        });
+      });
+    };
+
+    return {
+      sentence,
+      placeholders,
+      replacingWords,
+      getPlaceholders,
+      replacedSentences,
+      replaceSentence,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
